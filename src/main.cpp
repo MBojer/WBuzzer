51fcfa1420ab8b479c75d4110ed2c6819e4977e2
mbojer <morten.bojer@gmail.com>
2018-04-07 23:43:54 +0100

    fist commit

---
+++ src/main.cpp
@@ -0,0 +1,439 @@
+#include "Pitches.h"
+#include <Arduino.h>
+extern "C" {
+  #include "user_interface.h"
+}
+
+
+// ---------------------------------------- ArduinoOTA_Setup() ----------------------------------------
+bool ArduinoOTA_Active = false;
+
+
+// ------------------------------------------------------------ WiFi ------------------------------------------------------------
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <Ticker.h>
+
+WiFiClient WiFi_Client;
+
+const char* WiFi_SSID = "NoInternetHere";
+const char* WiFi_Password = "NoPassword1!";
+String WiFi_Hostname = "WBuzzer";
+
+WiFiEventHandler gotIpEventHandler;
+WiFiEventHandler disconnectedEventHandler;
+
+Ticker wifiReconnectTimer;
+#define WiFi_Reconnect_Delay 3 // in secounds
+
+
+// ------------------------------------------------------------ MQTT ------------------------------------------------------------
+#include <AsyncMqttClient.h>
+
+AsyncMqttClient MQTT_Client;
+Ticker mqttReconnectTimer;
+
+IPAddress MQTT_Broker(192, 168, 0, 2);
+unsigned long MQTT_Port = 1883;
+
+String MQTT_Device_ID = WiFi_Hostname;
+const char* MQTT_Username = "DasBoot";
+const char* MQTT_Password = "NoSinking";
+
+Ticker MQTT_KeepAlive_Ticker;
+unsigned long MQTT_KeepAlive_Delay = 60000;
+
+const byte MQTT_Subscribe_Topic_Number_Of = 2;
+String MQTT_Subscribe_Topic[MQTT_Subscribe_Topic_Number_Of] = {"/Boat/Settings/" + WiFi_Hostname + "/#", "/Boat/Buzzer/" + WiFi_Hostname};
+
+#define BoatAll 1
+#define BoatBuzzerHostname 2
+
+#define MQTT_Reconnect_Delay 2 // in secounds
+
+#define MQTT_Boot_Wait_For_Connection 15000
+
+
+// ---------------------------------------- Buzzer ----------------------------------------
+const int Buzzer_Pin = D1;
+
+Ticker Buzzer_Ticker;
+
+String Buzzer_Melody;
+
+
+// ############################################################ Buzzer_OFF() ############################################################
+void Buzzer_OFF() {
+
+  noTone(Buzzer_Pin);
+
+  Serial.println("Play OFF"); // rm
+
+} // Buzzer_OFF()
+
+
+// ############################################################ Buzzer_Play() ############################################################
+void Buzzer_Play() {
+
+  // Delay
+  if (Buzzer_Melody.substring(0, 1) == "-") {
+
+    noTone(Buzzer_Pin);
+
+    // Remove "-"
+    Buzzer_Melody = Buzzer_Melody.substring(1, Buzzer_Melody.length());
+
+    int Tone_Delay = Buzzer_Melody.substring(0, Buzzer_Melody.indexOf("-")).toInt();
+
+    Buzzer_Melody = Buzzer_Melody.substring(1, Buzzer_Melody.length());
+
+    Serial.println("Delay " + String(Tone_Delay)); // rm
+
+    Buzzer_Ticker.once_ms(Tone_Delay, Buzzer_Play);
+  }
+
+
+  // Play
+  else if (Buzzer_Melody.indexOf(",") != -1) {
+    int Tone = Buzzer_Melody.substring(0, Buzzer_Melody.indexOf(",")).toInt();
+    unsigned long Tone_Time = Buzzer_Melody.substring(Buzzer_Melody.indexOf(",") + 1, Buzzer_Melody.length()).toInt();
+
+    Buzzer_Melody = Buzzer_Melody.substring(Buzzer_Melody.indexOf(String(Tone_Time)) + String(Tone_Time).length(), Buzzer_Melody.length());
+
+    tone(Buzzer_Pin, Tone); // UNCOMMENT ME
+
+    Serial.print("Play: " + String(Tone)); // rm
+    Serial.println(" For: " + String(Tone_Time)); // rm
+
+    if (Buzzer_Melody != "") {
+      Buzzer_Ticker.once_ms(Tone_Time, Buzzer_Play);
+    }
+
+    else Buzzer_Ticker.once_ms(Tone_Time, Buzzer_OFF);
+  }
+
+} // Buzzer_Play()
+
+
+// ############################################################ Buzzer() ############################################################
+void Buzzer(String Topic, String Payload) {
+  if (Topic.indexOf(MQTT_Subscribe_Topic[BoatBuzzerHostname]) != -1) {
+    Buzzer_Melody = Payload;
+    Buzzer_Play();
+  }
+} // Buzzer()
+
+
+
+// ############################################################ KillKillKill() ############################################################
+void KillKillKill(String Topic, String Payload) {
+
+  if (Topic == "/Boat/All" && Payload == "KillKillKill") {
+
+    long Reboot_Delay = random(25000);
+
+    MQTT_Client.publish(String("/Boat/System/" + WiFi_Hostname).c_str(), 0, false, String("Someone went on a killing spree, rebooting in " + String(Reboot_Delay) + " ...").c_str());
+    Serial.println("Someone went on a killing spree, rebooting in" + String(Reboot_Delay));
+
+    unsigned long Suicide_At = millis() + Reboot_Delay;
+
+    while (millis() < Suicide_At) {
+      // Wait
+      delay(1);
+    }
+
+    ESP.restart();
+  }
+
+  else if (Topic == MQTT_Subscribe_Topic[1] && Payload == "KillKillKill") {
+
+    MQTT_Client.publish(String("/Boat/System/" + WiFi_Hostname).c_str(), 0, false, "Kill command issued, rebooting in 10");
+    Serial.println("Kill command issued, rebooting in 10");
+
+    unsigned long Suicide_At = millis() + 10000;
+
+    while (millis() < Suicide_At) {
+      // Wait
+    }
+
+    ESP.restart();
+  }
+
+} // KillKillKill()
+
+
+// ############################################################ UpTime_String() ############################################################
+String Uptime_String() {
+
+  unsigned long Uptime_Now = millis();
+
+  unsigned long Uptime_Days = Uptime_Now / 86400000;
+  if (Uptime_Days != 0) Uptime_Now -= Uptime_Days * 86400000;
+
+  unsigned long Uptime_Hours = Uptime_Now / 3600000;
+  if (Uptime_Hours != 0) Uptime_Now -= Uptime_Hours * 3600000;
+
+  unsigned long Uptime_Minutes = Uptime_Now / 60000;
+  if (Uptime_Minutes != 0) Uptime_Now -= Uptime_Minutes * 60000;
+
+  unsigned long Uptime_Secunds = Uptime_Now / 1000;
+  if (Uptime_Secunds != 0) Uptime_Now -= Uptime_Secunds * 1000;
+
+  String Uptime_String = "Up for ";
+
+  if (Uptime_Days != 0) {
+    if (Uptime_Days == 1) Uptime_String += String(Uptime_Days) + " day ";
+    else Uptime_String += String(Uptime_Days) + " days ";
+  }
+
+  if (Uptime_Hours != 0) {
+    if (Uptime_Hours == 1) Uptime_String += String(Uptime_Hours) + " hour ";
+    else Uptime_String += String(Uptime_Hours) + " hours ";
+  }
+
+  if (Uptime_Minutes != 0) Uptime_String += String(Uptime_Minutes) + " min ";
+  if (Uptime_Secunds != 0) Uptime_String += String(Uptime_Secunds) + " sec ";
+  if (Uptime_Now != 0) Uptime_String += String(Uptime_Now) + " ms ";
+
+  return Uptime_String;
+
+} // Uptime_String()
+
+
+// ############################################################ connectToWifi() ############################################################
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi ...");
+  WiFi.begin(WiFi_SSID, WiFi_Password);
+}
+
+
+// ############################################################ onMqttConnect() ############################################################
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT");
+
+  if (MQTT_Subscribe_Topic_Number_Of > 0) {
+
+    for (byte i = 0; i < MQTT_Subscribe_Topic_Number_Of; i++) {
+      if (MQTT_Client.subscribe(MQTT_Subscribe_Topic[i].c_str(), 0)) {
+        Serial.println("Subscribing to Topic: " + MQTT_Subscribe_Topic[i] + "  ... OK");
+      }
+
+      else Serial.println("Subscribing to Topic: " + MQTT_Subscribe_Topic[i] + "  ... FAILED");
+    }
+  }
+} // onMqttConnect()
+
+
+// ############################################################ onMqttSubscribe() ############################################################
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {}
+
+
+// ############################################################ connectToMqtt() ############################################################
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT ...");
+  MQTT_Client.connect();
+}
+
+
+// ############################################################ onMqttDisconnect() ############################################################
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(MQTT_Reconnect_Delay, connectToMqtt);
+  }
+}
+
+
+// ############################################################ onMqttUnsubscribe() ############################################################
+void onMqttUnsubscribe(uint16_t packetId) {}
+
+
+// ############################################################ MQTT_KeepAlive() ############################################################
+void MQTT_KeepAlive() {
+
+  String Send_String = Uptime_String() + " Free Memory: " + String(system_get_free_heap_size());
+
+  MQTT_Client.publish(String("/Boat/KeepAlive/" + WiFi_Hostname).c_str(), 0, false, Send_String.c_str());
+
+} // MQTT_KeepAlive()
+
+
+// ############################################################ MQTT_Settings() ############################################################
+void MQTT_Settings(String Topic, String Payload) {
+
+  if (Topic.indexOf("/Boat/Settings/" + WiFi_Hostname) == -1) return;
+
+  // ############### MQTTKeepAlive ###############
+  if (Topic.indexOf("MQTTKeepAlive") != -1) {
+
+    if (Payload.toInt() != MQTT_KeepAlive_Delay) {
+
+      MQTT_KeepAlive_Ticker.detach();
+
+      MQTT_KeepAlive_Delay = Payload.toInt();
+
+      MQTT_KeepAlive_Ticker.attach_ms(MQTT_KeepAlive_Delay, MQTT_KeepAlive);
+
+      Serial.println("KeepAlive change to: " + String(MQTT_KeepAlive_Delay));
+    }
+  } // MQTTKeepAlive
+
+
+} // MQTT_Settings
+
+
+// ############################################################ onMqttMessage() ############################################################
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+
+  if (ArduinoOTA_Active == true) return;
+
+  MQTT_Settings(topic, payload);
+
+  Buzzer(topic, payload);
+
+  KillKillKill(topic, payload);
+
+} // Settings
+
+
+// ############################################################ IPtoString() ############################################################
+String IPtoString(IPAddress IP_Address) {
+
+  String Temp_String = String(IP_Address[0]) + "." + String(IP_Address[1]) + "." + String(IP_Address[2]) + "." + String(IP_Address[3]);
+
+  return Temp_String;
+
+} // IPtoString
+
+
+// ############################################################ ArduinoOTA_Setup() ############################################################
+void ArduinoOTA_Setup() {
+
+  ArduinoOTA.setHostname(WiFi_Hostname.c_str());
+  ArduinoOTA.setPassword("StillNotSinking");
+
+  ArduinoOTA.onStart([]() {
+
+    MQTT_Client.publish(String("/Boat/System/" + WiFi_Hostname).c_str(), 0, false, "ArduinoOTA ... Started");
+    ArduinoOTA_Active = true;
+    MQTT_KeepAlive_Ticker.detach();
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    MQTT_Client.publish(String("/Boat/System/" + WiFi_Hostname).c_str(), 0, false, "ArduinoOTA ... End");
+    ArduinoOTA_Active = false;
+    Serial.println("End");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    ArduinoOTA_Active = false;
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
+} // ArduinoOTA_Setup()
+
+
+// ############################################################ setup() ############################################################
+void setup() {
+
+  // ------------------------------ Random Seed ------------------------------
+  randomSeed(analogRead(0));
+
+  // ------------------------------ Pins ------------------------------
+  // Buzzer
+  pinMode(Buzzer_Pin, OUTPUT);
+
+
+  // ------------------------------ Serial ------------------------------
+  Serial.setTimeout(50);
+  Serial.begin(115200);
+
+
+  // ------------------------------ MQTT ------------------------------
+  MQTT_Client.onConnect(onMqttConnect);
+  MQTT_Client.onDisconnect(onMqttDisconnect);
+  MQTT_Client.onUnsubscribe(onMqttUnsubscribe);
+  MQTT_Client.onMessage(onMqttMessage);
+  MQTT_Client.onSubscribe(onMqttSubscribe);
+
+  MQTT_Client.setServer(MQTT_Broker, MQTT_Port);
+  MQTT_Client.setCredentials(MQTT_Username, MQTT_Password);
+
+
+  // ------------------------------ WiFi ------------------------------
+  Serial.println("WiFi SSID: " + String(WiFi_SSID));
+
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(WiFi_Hostname);
+
+  gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP& event) {
+    Serial.println("Connected to Wi-Fi - IP: " + IPtoString(WiFi.localIP()));
+    ArduinoOTA_Setup();
+    connectToMqtt();
+  });
+
+  disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& event) {
+    Serial.println("Disconnected from Wi-Fi");
+    mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+    wifiReconnectTimer.once(WiFi_Reconnect_Delay, connectToWifi);
+  });
+
+  connectToWifi();
+
+
+  // ------------------------------ MQTT KeepAlive ------------------------------
+  MQTT_KeepAlive_Ticker.attach_ms(MQTT_KeepAlive_Delay, MQTT_KeepAlive);
+
+
+  // ------------------------------ Wait for MQTT ------------------------------
+  unsigned long MQTT_Boot_Wait_Timeout_At = millis() + MQTT_Boot_Wait_For_Connection;
+
+  while (MQTT_Client.connected() == false) {
+
+    if (MQTT_Boot_Wait_Timeout_At < millis()) break;
+
+    delay(250);
+  }
+
+  // ------------------------------ Boot End ------------------------------
+  MQTT_Client.publish(String("/Boat/System/" + WiFi_Hostname).c_str(), 0, false, String("Booting. Free Memory: " + String(system_get_free_heap_size())).c_str());
+  Serial.println("Boot done");
+
+
+} // setup()
+
+
+// ############################################################ loop() ############################################################
+void loop() {
+
+  while (ArduinoOTA_Active == true) {
+    ArduinoOTA.handle();
+  }
+  ArduinoOTA.handle();
+
+} // loop()
